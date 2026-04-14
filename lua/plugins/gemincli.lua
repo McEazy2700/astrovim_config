b1e34870c68941f4a9cc4e6abbd57b9105a4231f
Ezekiel Victor <codeepoch@gmail.com>
2026-04-14 17:50:19 +0100

    Feat: add gemini and claudecode

---
+++ lua/plugins/gemincli.lua
@@ -0,0 +1,28 @@
+return {
+  "mceazy2700/gemini-cli.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "folke/snacks.nvim",
+  },
+  opts = {
+    terminal = {
+      size = 120,
+    },
+    -- See Configuration section for options
+  },
+  keys = {
+    { "<leader>a", nil, desc = "AI Agents" },
+    { "<leader>ai", "<cmd>Gemini<cr>", desc = "Toggle Gemini", mode = { "n" } },
+    -- { "<leader>agr", "<cmd>GeminiResume<cr>", desc = "Resume Gemini" },
+    -- { "<leader>agm", "<cmd>GeminiSelectModel<cr>", desc = "Select Gemini model" },
+    { "<leader>ai", "<cmd>GeminiAsk<cr>", desc = "Ask Gemini", mode = { "v" } },
+    -- {
+    --   "<leader>agb",
+    --   function() require("gemini-cli.integrations").add_to_context(vim.api.nvim_buf_get_name(0)) end,
+    --   desc = "Add current buffer to Gemini context",
+    -- },
+    -- -- Diff management (when in a (proposed) buffer)
+    -- { "<leader>agy", "<cmd>GeminiDiffAccept<cr>", desc = "Accept diff" },
+    -- { "<leader>agn", "<cmd>GeminiDiffDeny<cr>", desc = "Deny diff" },
+  },
+}
